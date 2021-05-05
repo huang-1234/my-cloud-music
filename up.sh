f891e5e962cf53976df2a38037bab1699a609178
hsq <137707531@qq.com>
2021-05-05 14:05:16 +0800

    achieve app.Recommend and con.slider in chapter2

--- up.sh
+++ up.sh
@@ -9,7 +9,7 @@ git commit -m 'achieve app.Recommend and con.slider in chapter2'
 
 git remote -v
 
-# git remote add origin git@github.com:huang-1234/my-cloud-music.git
+git remote add origin git@github.com:huang-1234/my-cloud-music.git
 
 git pull
 
